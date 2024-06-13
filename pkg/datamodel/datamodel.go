8fd3f6576c33ec75c561a42b36428c85aae09a31
Micah Sherr <msherr@cs.georgetown.edu>
2024-06-12 22:17:54 -0400

    untested

--- pkg/datamodel/datamodel.go
+++ pkg/datamodel/datamodel.go
@@ -10,3 +10,18 @@ type ProcessJobStruct struct {
 	JobNo   JobNoType
 	CmdLine string
 }
+
+// JsonCommandStruct represents a command to be executed.
+type JsonCommandStruct struct {
+	TimeoutInSecs float32  `json:"timeout"` // anything positive will be considered a timeout
+	Cmd           string   `json:"cmd"`
+	Args          []string `json:"args"`
+	StdoutFile    string   `json:"stdout"`
+	StderrFile    string   `json:"stderr"`
+}
+
+// JsonKillStruct represents a job to be killed.
+// note that  if job == -1, then all jobs will be killed
+type JsonKillStruct struct {
+	JobNo JobNoType `json:"job"`
+}
